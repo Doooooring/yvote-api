e0a9c55317f7db4dc84d8b2a9eda0c002e237190
doring <2017132021@yonsei.ac.kr>
2025-01-10 14:29:40 +0900

    fix auth guard

--- src/auth/admin/admin.guard.ts
+++ src/auth/admin/admin.guard.ts
@@ -7,7 +7,6 @@ export class AdminGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    return true;
     try {
       const request = context.switchToHttp().getRequest();
       const token = request.cookies?.['access_token'];
