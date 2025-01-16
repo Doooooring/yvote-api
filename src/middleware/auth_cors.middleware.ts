a153e0f3ee134118c01355f10f63334a3da665ee
doring <2017132021@yonsei.ac.kr>
2025-01-16 10:43:36 +0900

    delete console

--- src/middleware/auth_cors.middleware.ts
+++ src/middleware/auth_cors.middleware.ts
@@ -5,10 +5,6 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class AuthCorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('--- AuthCorsMiddleware 시작 ---');
-    console.log(`요청 메소드: ${req.method}`);
-    console.log(`요청 경로: ${req.path}`);
-    console.log(`Origin 헤더: ${req.headers.origin}`);
     const corsOptions = {
       origin: [
         'http://localhost:3001',
@@ -25,9 +21,6 @@ export class AuthCorsMiddleware implements NestMiddleware {
         console.log(err);
         return res.status(500).json({ message: 'CORS 설정 오류' });
       }
-      console.log('AuthCorsMiddleware를 통한 CORS 설정 성공');
-      console.log('응답 헤더 (CORS 관련):', res.getHeaders());
-      console.log('--- AuthCorsMiddleware 종료 ---\n');
       next();
     });
   }
