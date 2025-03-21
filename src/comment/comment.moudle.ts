74bf7abcd3ef69636525638c595d00fcc2006480
kang seok <sekeipost@naver.com>
2025-03-22 00:34:26 +0900

    add open ai module

--- src/comment/comment.moudle.ts
+++ src/comment/comment.moudle.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { OpenAIModule } from 'src/openai/openai.module';
 import { OpenAIService } from 'src/openai/openai.service';
 import { RepositoryModule } from 'src/repository/repository.module';
+import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
 
 @Module({
-  controllers: [Comment],
+  controllers: [CommentController],
   providers: [CommentService, OpenAIService],
   imports: [RepositoryModule, OpenAIModule],
   exports: [],
