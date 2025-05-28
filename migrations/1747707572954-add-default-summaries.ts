88cf33d4151428382298577a4ac5e24dd476509f
kang seok <sekeipost@naver.com>
2025-05-28 12:19:45 +0900

    fix table name

--- migrations/1747707572954-add-default-summaries.ts
+++ migrations/1747707572954-add-default-summaries.ts
@@ -3,14 +3,14 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AddDefaultSummaries1747707572954 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            INSERT INTO newsSummary (summary, commentType, newsId)
+            INSERT INTO NEWSUMMARY (summary, commentType, newsId)
             SELECT '' AS summary, CC.commentType, CC.newsId
             FROM 
             (
                 select C.newsId newsId, C.commentType commentType from Comment C 
                 group by C.newsId, C.commentType    
             ) CC
-            LEFT JOIN NewsSummary NS
+            LEFT JOIN NEWSSUMMARY NS
             ON CC.newsId = NS.newsId AND CC.commentType = NS.commentType
             WHERE NS.newsId IS NULL
         `);
