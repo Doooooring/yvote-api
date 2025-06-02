85f916272647f6f5d973ec34fccb864b327d93b7
kang seok <sekeipost@naver.com>
2025-06-02 12:15:36 +0900

    chore

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -19,14 +19,17 @@ export class CommentController {
   async getCommentSummary(@Param('id') id: number) {
     const comment = await this.commentService.getCommentByCommentId(id);
 
-    const summary = await this.openAiService.getOpenAI([
-      {
-        role: 'system',
-        content:
-          '글에서 뉴스 독자들이 읽을만한 부분을 쉽고 짧게 요약해 줘. 내용과 관계 없는 쓸데 없는 말은 빼고.',
-      },
-      { role: 'user', content: comment.comment },
-    ]);
+    const summary = await this.openAiService.getOpenAI(
+      [
+        {
+          role: 'system',
+          content:
+            '글에서 뉴스 독자들이 읽을만한 부분을 쉽고 짧게 요약해 줘. 내용과 관계 없는 쓸데 없는 말은 빼고.',
+        },
+        { role: 'user', content: comment.comment },
+      ],
+      'grok-2',
+    );
 
     return summary;
   }
