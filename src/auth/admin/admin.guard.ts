3d950ad0ec000e79dca7cd285c45be35d9f705db
doring <2017132021@yonsei.ac.kr>
2025-06-09 13:12:36 +0900

    add token authorization logic

--- src/auth/admin/admin.guard.ts
+++ src/auth/admin/admin.guard.ts
@@ -1,25 +1,54 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private configServie: ConfigService,
+    private jwtService: JwtService,
+  ) {}
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
+  async checkCookieHeader(req: Request) {
+    const token = req.cookies?.['access_token'];
+
+    if (!token) {
+      throw new Error('TokenNotExist');
+    }
     try {
-      const request = context.switchToHttp().getRequest();
-      const token = request.cookies?.['access_token'];
-
-      if (!token) {
-        throw new Error('TokenNotExist');
-      }
-
-      const payload = await this.jwtService.verifyAsync(token);
+      await this.jwtService.verifyAsync(token);
       return true;
     } catch (e) {
-      throw Error(e);
+      throw new Error('TokenInvalid');
     }
+  }
+
+  async checkAuthToken(req: Request) {
+    const token = this.extractTokenFromHeader(req);
+    const adminToken = this.configServie.get('YVOTE_ADMIN_Token');
+
+    if (!token) {
+      throw new Error('TokenNotExist');
+    }
+
+    if (token !== adminToken) {
+      throw new Error('TokenInvalid');
+    }
+
+    return true;
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+
+    await Promise.any([
+      this.checkCookieHeader(request),
+      Promise.resolve().then(() => this.checkAuthToken(request)),
+    ]).catch((error) => {
+      throw new Error(error.message);
+    });
+    return true;
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
